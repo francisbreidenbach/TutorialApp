8cfe602c597a99f7391f3e2cb2fa8c03a291ef1d
Francis Breidenbach <francis.breidenbach@dws.la>
2021-12-09 17:11:42 -0800

    Add TextInput

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput value="Francis Breidenbach"/>
       <Text>Welcome to the Tutorial App for Udemy Course!</Text>
       <StatusBar style="auto" />
     </View>
